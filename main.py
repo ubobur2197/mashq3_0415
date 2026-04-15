622bf11ce888379753bca2071b01cb55a440873a
Bobur <ubobur2197@gmail.com>
2026-04-15 11:38:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# 1
+royxat = [3, 7, 2, 9, 12, 5]
+katta_5 = [i for i in royxat if i > 5]
+print(katta_5)
+
+
+# 2
+royxat = [1, 4, 6, 8, 3, 10]
+juft_katta_5 = [i for i in royxat if i % 2 == 0 and i > 5]
+print(juft_katta_5)
+
+
+# 3
+royxat = [0, -1, 5, -3, 8, 2]
+nol_emas = [i for i in royxat if i != 0]
+print(nol_emas)
+
+
+# 4
+royxat = [11, 22, 33, 44, 55]
+uchga_bolinadi = [i for i in royxat if i % 3 == 0]
+print(uchga_bolinadi)
+
+
+# 5
+royxat = [2, 3, 4, 5, 6, 7]
+kvadrat = [i**2 for i in royxat]
+print(kvadrat)
